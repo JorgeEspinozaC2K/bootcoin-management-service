abcd8f94a60479dd5fd5b36ee6acf807a975aa42
Jorge Andres Espinoza Canales <jorge.espinoza.c22@gmail.com>
2022-05-12 16:07:09 -0500

    Kaf

---
+++ src/main/java/com/bootcoin/management/service/app/BootcoinManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bootcoin.management.service.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcoinManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcoinManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcoin/management/service/app/BootcoinManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcoin.management.service.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcoinManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
